e1d4368a9a2b7b3452ee19eb24b394d33bdb7e1a
Roman Aebi <unity2.sae@gmail.com>
2023-02-22 21:14:32 +0100

    test

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,7 @@ namespace DefaultNamespace {
         [SerializeField] private Transform anchor;
         private void Update() {
             transform.RotateAround (anchor.transform.position, Vector3.up, 30 * Time.deltaTime);
+            
         }
     }
 }
