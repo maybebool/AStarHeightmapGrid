da8f167dde3d34a8d1813cd20adda6a233e6a8cc
Roman Aebi <unity2.sae@gmail.com>
2023-03-08 14:36:12 +0100

    changing directory

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,20 +2,19 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace DefaultNamespace {
-    public class CameraMovement : MonoBehaviour {
 
-        [SerializeField] private Transform anchor;
-        public float multiplier = 0;
-        private float _time;
-        private void Update() {
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField] private Transform anchor;
+    public float multiplier = 0;
+    private float _time;
+    private void Update() {
             
-            transform.RotateAround (anchor.transform.position, Vector3.up, 30 * Time.deltaTime * multiplier);
-        }
+        transform.RotateAround (anchor.transform.position, Vector3.up, 30 * Time.deltaTime * multiplier);
+    }
 
 
-        public void SliderMultiplier(float adjustingSpeed) {
-            multiplier = adjustingSpeed;
-        }
+    public void SliderMultiplier(float adjustingSpeed) {
+        multiplier = adjustingSpeed;
     }
 }
